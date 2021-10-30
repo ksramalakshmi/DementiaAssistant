a8f82c9b3a05008e9abde966d6a01725c933282a
K S Ramalakshmi <rlsri2305@gmail.com>
2021-10-30 16:59:06 +0530

    Add files via upload
    Code Repository for the Personal Assistant Robot and Smart Specs designed for Dementia affected. A technical solution.

---
+++ README.md
@@ -0,0 +1,14 @@
+#Problem Statement
+The modern age can be divided into two Before Covid, After Covid.
+	Everyone was running in life, everyone had so many things to do and no time to breathe but then everything stopped with that one day. The whole world stopped working and that’s when people actually took some time to relax but after few days it became months people became restless, people were so bored and got anxiety, depression, and loneliness. And now when things got normal everyone are back on track. 
+	But just take a step back and think just for 3 months we can’t be idle think of people who have dementia, who is paralyzed, who are disabled. This is their entire life they feel depressed most of the time lonely often and also they feel lost when the whole world is running. With the modernization of technology, it is not hard to find solutions to these problems.
+	Dementia is an impaired ability to remember, think, or make decisions that interfere with doing everyday activities. As cases increase, the cost of living increases for them (through meds, daycare nurses, etc.). Patients with even mild cases need constant care and reminders.
+	Worldwide, around 55 million people have dementia, with over 60% living in low- and middle-income countries. As the proportion of older people in the population is increasing in nearly every country, this number is expected to rise to 78 million in 2030 and 139 million in 2050.
+	When we ask these people if the disease making them depressed the answer is NO they think the feel of them being dependent on another person for simple things is making their life depressing and there are so many gadgets in the world now are available to make them independent but everything is expensive so we are trying to make the low-cost solution for helping people with dementia to live their life independently.
+
+#Solution
+			“Energy Cannot Be Created nor Destroyed”
+Similarly technology also, we have everything we need and what is called new technology is advancement/modification/combination of previous technology.
+	We are gonna make a low-cost smart spectacle that will act as a caretaker for the person with dementia, which will remind things like what to do and shows things how to do and also guides them if they want to go somewhere and also maintains their health record along with SOS in case if they fall on the ground or in any type of emergency.
+	Along with this we are also gonna make a personal assistant robot which converses with them, understands their emotions and provides moral support and also helps them to be in touch with their near ones using telepresence technology.
+	We will be using the fusion of AI, Robotics, NLP, Blockchain, and image processing to achieve this at a low cost but with max efficiency.
---
+++ hsv.py
@@ -0,0 +1,169 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+import serial
+import pyzbar.pyzbar as pyzbar
+
+from datetime import datetime
+import time
+
+ser = serial.Serial('COM4', 9600, timeout=1)
+move=0
+barcodeData=0
+
+from datetime import datetime
+
+morn = 0
+temp = 0
+cn=0
+b = 0
+detect = 7
+value = 0
+t = 0
+
+#cloud receive
+import csv
+import receive
+import os
+import speak
+from playsound import playsound
+
+while True:
+    timenow = datetime.now()
+    current_time = timenow.strftime("%H:%M:%S")
+    print(current_time[1])
+
+    if current_time[1] == "7" and t == 0:
+        move = 1
+        detect=7
+        morn=1
+        t=1
+        
+    if morn == 0:
+
+        b = receive.getMessage()
+        print("morning",b)
+
+        if temp  != b:
+            value = b 
+            temp = b
+        print('value',value)
+
+        if value == 'mo':
+            print("mo")
+            os.system("robo.mp4")
+            value=0
+            move = 0
+
+        if value == 'j' and cn>0:
+            print('j')
+            cn=cn+1
+            speak.speak_text("What do you call a train carrying bubblegum? A chew chew train")
+            os.system("python smile.py")
+            value = 0
+            move = 0
+
+        if value == 'mu':
+            print("mu")
+            playsound('wake.mp3')
+            print('playing sound using  playsound')
+            value = 0
+            move = 0
+        
+        if str(value)=='1':
+            move=1
+            if barcodeData != "8":
+                move=1
+            if barcodeData == "8":
+                move=0
+        if str(value)=='2':
+            if barcodeData != "7":
+                move=1
+
+            if barcodeData == "7":
+                move=0
+
+    print('r')  
+
+    _, frame = cap.read()
+    barcodes = pyzbar.decode(frame)
+    if len(barcodes)>0:
+        for barcode in barcodes:
+            (x, y, w, h) = barcode.rect
+            barcodeData = barcode.data.decode('utf-8')
+            barcodeType = barcode.type
+            text = "{} ( {} )".format(barcodeData, barcodeType)
+            print(barcodeData, morn)
+            if morn == 1:
+                
+                if str(detect) == barcodeData:
+                    print("detector bar code")
+                    ser.write(b'w')
+                    ser.write(b'w')
+                    os.system("python wakeup.py")
+                    move=0
+                    morn =0
+                else:
+                    ser.write(b'w')
+                    print("left bar code")
+                    ser.write(b'w')
+                    move = 1
+            
+    print('l')
+    print('action')
+
+    frame = cv2.blur(frame, (20,20))
+    cv2.imshow("blred",frame)
+
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    low_green = np.array([35, 100, 50])
+    high_green = np.array([85, 255,255])
+    green_mask = cv2.inRange(hsv_frame, low_green, high_green)
+    green = cv2.bitwise_and(frame, frame, mask=green_mask)
+
+    kernel = np.ones((5,5), np.uint8)
+    mask = cv2.dilate(green, kernel, iterations=1)
+    gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    edged = cv2.Canny(gray, 40, 500)
+  
+    cnts,_=cv2.findContours(edged,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    center = None
+
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)  
+        ((x, y), radius) = cv2.minEnclosingCircle(c) 
+        center=int(x),int(y)
+        print(move)
+        if move==1:
+            if int(x)<150:
+                ser.write(b'a')
+                time.sleep(0.15)
+            if int(x)>150 and int(x)<440:
+                ser.write(b'w')
+                time.sleep(0.15)
+            if int(x)>440:
+                ser.write(b'd')
+                time.sleep(0.15)
+
+    if len(cnts) == 0 and move==1:
+        ser.write(b'a')
+        time.sleep(0.2)
+
+    cv2.imshow("Green", green)
+
+    key = cv2.waitKey(1)
+
+    if key == 27:
+        ser.close()
+        break
+
+    b = receive.getMessage()
+
+    if current_time[1] == "8":
+        os.system("python pose.py")
+        
+    if current_time[1] == "9":
+        speak.speak_text("take your medicine")
